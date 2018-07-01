fa696490d2d62296593a84a711b8dd844ab6e537
ChenJianPing01 <361512960@qq.com>
2018-07-01 12:19:32 +0800

    Initial commit

---
+++ js/spa.js
@@ -0,0 +1,12 @@
+/* spa.js */
+
+var spa = (function () {
+    var initModule = function ($container) {
+        spa.shell.initModule($container);
+    }
+
+    return { initModule: initModule };
+}()); 
+
+
+
